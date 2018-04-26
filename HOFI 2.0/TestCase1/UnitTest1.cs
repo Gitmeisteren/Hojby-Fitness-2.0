d90ab3cdd2e207f38f686b0ed524c5697a5221c9
Biest1155 <mikk934a@edu.eal.dk>
2018-04-26 13:33:29 +0200

    Made Unittest for testing SQL connection

--- HOFI 2.0/TestCase1/UnitTest1.cs
+++ HOFI 2.0/TestCase1/UnitTest1.cs
@@ -5,26 +5,15 @@ using ViewModel;
 using System.Data;
 using System.Data.SqlClient;
 
+
 namespace TestCase1
 {
     [TestClass]
     public class UnitTest1
     {
-        Controller _controller = new Controller();
-        SQLDatabaseConnectionPoint sqlDatabaseConnectionPoint = new SQLDatabaseConnectionPoint();
-        private static string testConString = "Server= den1.mssql5.gear.host; Database= hofi; User ID = hofi; Password= Qg9OG4l~v-06;";
-        SqlConnection testCon = new SqlConnection(testConString);
-        
-        [TestMethod] //Tester forbindelsen til SQLDatabasen.
-        public void TestSqlConnection()
+        [TestMethod]
+        public void TestMethod1()
         {
-            testCon.Open();
-            bool ConnecTest = false;
-            if (testCon.State == ConnectionState.Open)
-            {
-                ConnecTest = true;
-            }
-            Assert.AreEqual(true, ConnecTest);
         }
     }
 }
